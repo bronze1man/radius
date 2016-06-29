6042fd861390b4e08aa19bbd25b61dd2a4278ad8
Sergey Leschenko <sergle.ua@gmail.com>
2016-06-29 18:40:22 +0300

    added support for PoD and CoA packet codes

--- PacketCode.go
+++ PacketCode.go
@@ -15,29 +15,38 @@ const (
 	AccessChallenge    PacketCode = 11
 	StatusServer       PacketCode = 12 //(experimental)
 	StatusClient       PacketCode = 13 //(experimental)
+	DisconnectRequest  PacketCode = 40
+	DisconnectAccept   PacketCode = 41
+	DisconnectReject   PacketCode = 42
+	CoARequest         PacketCode = 43
+	CoAAccept          PacketCode = 44
+	CoaReject          PacketCode = 45
 	Reserved           PacketCode = 255
 )
 
+var packetCodeName = map[PacketCode]string{
+	AccessRequest:      "AccessRequest",
+	AccessAccept:       "AccessAccept",
+	AccessReject:       "AccessReject",
+	AccountingRequest:  "AccountingRequest",
+	AccountingResponse: "AccountingResponse",
+	AccessChallenge:    "AccessChallenge",
+	StatusServer:       "StatusServer",
+	StatusClient:       "StatusClient",
+	DisconnectRequest:  "DisconnectRequest",
+	DisconnectAccept:   "DisconnectAccept",
+	DisconnectReject:   "DisconnectReject",
+	CoARequest:         "CoARequest",
+	CoAAccept:          "CoAAccept",
+	CoaReject:          "CoaReject",
+	Reserved:           "Reserved",
+}
+
 func (p PacketCode) String() string {
-	switch p {
-	case AccessRequest:
-		return "AccessRequest"
-	case AccessAccept:
-		return "AccessAccept"
-	case AccessReject:
-		return "AccessReject"
-	case AccountingRequest:
-		return "AccountingRequest"
-	case AccountingResponse:
-		return "AccountingResponse"
-	case AccessChallenge:
-		return "AccessChallenge"
-	case StatusServer:
-		return "StatusServer"
-	case StatusClient:
-		return "StatusClient"
-	case Reserved:
-		return "Reserved"
+	name, ok := packetCodeName[p]
+	if ok {
+		return name
 	}
+
 	return "unknown packet code " + strconv.Itoa(int(p))
 }
